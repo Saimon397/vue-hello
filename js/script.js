7535fb57a33b3e96e2c84ddaac688df1751f3aa4
Saimon397 <simonecarrabba@gmail.com>
2022-10-26 14:57:05 +0200

    finito.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const { createApp } = Vue;
+createApp({
+    data() {
+        return {
+            message: 'Hello World by Vue !!!',
+            img: 'https://cdn.discordapp.com/attachments/1026540296927989800/1034812151782703104/logo.png'
+
+        }
+    }
+}).mount('#Hello')
